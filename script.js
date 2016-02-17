15f1e837e7e4be7a4293811f5b839ac6a4ac7bb3
Stephen Clark <stephen@californiaclarks.com>
2016-02-17 13:37:12 -0500

    Drawing cars

--- script.js
+++ script.js
@@ -3,6 +3,81 @@ var scale = 40;
 var pos = 0;
 var frames;
 
+function drawCar(ctx, i, j, dir, reverse) {
+  var reverseDict = {"l": "r", "r": "l", "u": "d", "d": "u"};
+  if (reverse) dir = reverseDict[dir];
+  ctx.fillStyle = "#"+((1<<24)*Math.random()|0).toString(16); //"#07c";
+  ctx.beginPath();
+  switch(dir){
+    case "l":
+      ctx.moveTo((j+0.875)*scale, (i+0.75)*scale);
+      ctx.arc((j+0.375)*scale, (i+0.5)*scale, scale*0.25, 0.5*Math.PI, 1.5*Math.PI);
+      ctx.lineTo((j+0.875)*scale, (i+0.25)*scale);
+      break;
+    case "r":
+      ctx.moveTo((j+0.125)*scale, (i+0.25)*scale);
+      ctx.arc((j+0.625)*scale, (i+0.5)*scale, scale*0.25, 1.5*Math.PI, 0.5*Math.PI);
+      ctx.lineTo((j+0.125)*scale, (i+0.75)*scale);
+      break;
+    case "u":
+      ctx.moveTo((j+0.75)*scale, (i+0.875)*scale);
+      ctx.arc((j+0.5)*scale, (i+0.375)*scale, scale*0.25, 0*Math.PI, 1*Math.PI, true);
+      ctx.lineTo((j+0.25)*scale, (i+0.875)*scale);
+      break;
+    case "d":
+      ctx.moveTo((j+0.25)*scale, (i+0.125)*scale);
+      ctx.arc((j+0.5)*scale, (i+0.625)*scale, scale*0.25, 1*Math.PI, 0*Math.PI, true);
+      ctx.lineTo((j+0.75)*scale, (i+0.125)*scale);
+      break;
+  }
+  ctx.closePath();
+  ctx.fill();
+}
+
+function drawParkingSpace(ctx, i, j, dir) {
+  switch (dir) {
+  case "l":
+    ctx.fillStyle = "#f7df1e";
+    ctx.fillRect(j*scale, i*scale, scale, scale);
+    ctx.fillStyle = "#A9A9A9";
+    ctx.fillRect(j*scale, i*scale + 5, scale-5, scale-10);
+    break;
+  case "r":
+    ctx.fillStyle = "#f7df1e";
+    ctx.fillRect(j*scale, i*scale, scale, scale);
+    ctx.fillStyle = "#A9A9A9";
+    ctx.fillRect(j*scale+5, i*scale + 5, scale, scale-10);
+    break;
+  case "u":
+    ctx.fillStyle = "#f7df1e";
+    ctx.fillRect(j*scale, i*scale, scale, scale);
+    ctx.fillStyle = "#A9A9A9";
+    ctx.fillRect(j*scale+5, i*scale + 5, scale, scale-10);
+    break;
+  case "d":
+    ctx.fillStyle = "#f7df1e";
+    ctx.fillRect(j*scale, i*scale, scale, scale);
+    ctx.fillStyle = "#A9A9A9";
+    ctx.fillRect(j*scale+5, i*scale + 5, scale, scale-10);
+    break;
+  }
+}
+
+function drawRoad(ctx, i, j, dir) {
+  ctx.fillStyle = "#000";
+  ctx.fillRect(j*scale, i*scale, scale, scale);
+}
+
+function drawWall(ctx, i, j) {
+  ctx.fillStyle = "#f00";
+  ctx.fillRect(j*scale, i*scale, scale, scale);
+}
+
+function drawTarget(ctx, i, j) {
+  ctx.fillStyle = "#0c7";
+  ctx.fillRect(j*scale, i*scale, scale, scale);
+}
+
 function draw(parkingLot) {
   var height = parkingLot.length;
   var width = parkingLot[0].length;
@@ -12,58 +87,27 @@ function draw(parkingLot) {
   var ctx = canvas.getContext("2d");
   for (var i = 0; i < height; i++) {
     for (var j = 0; j < width; j++) {
-      var currentSpace = parkingLot[i][j]
-      switch (currentSpace.substring(0, 1)) {
-        case "w":
-      	  ctx.fillStyle = "#f00";
-      	  ctx.fillRect(j*scale, i*scale, scale, scale);
-          break;
-      	case "e":
-      	case "x":
-        case "r":
-      	  ctx.fillStyle = "#000";
-      	  ctx.fillRect(j*scale, i*scale, scale, scale);
-          break;
-        case "p":
-          switch(currentSpace.substring(1,2)){
-            case "l":
-              ctx.fillStyle = "#f7df1e";
-              ctx.fillRect(j*scale, i*scale, scale, scale);
-              ctx.fillStyle = "#A9A9A9";
-              ctx.fillRect(j*scale, i*scale + 5, scale-5, scale-10);
-              break;
-            case "r":
-              ctx.fillStyle = "#f7df1e";
-              ctx.fillRect(j*scale, i*scale, scale, scale);
-              ctx.fillStyle = "#A9A9A9";
-              ctx.fillRect(j*scale+5, i*scale + 5, scale, scale-10);
-              break;             
-            case "u":
-              ctx.fillStyle = "#f7df1e";
-              ctx.fillRect(j*scale, i*scale, scale, scale);
-              ctx.fillStyle = "#A9A9A9";
-              ctx.fillRect(j*scale+5, i*scale + 5, scale, scale-10);
-              break;
-            case "d":
-              ctx.fillStyle = "#f7df1e";
-              ctx.fillRect(j*scale, i*scale, scale, scale);
-              ctx.fillStyle = "#A9A9A9";
-              ctx.fillRect(j*scale+5, i*scale + 5, scale, scale-10);
-              break;
-            default:
-              ctx.fillStyle = "#A9A9A9";
-              ctx.fillRect(j*scale+5, i*scale + 5, scale, scale-10);
-              break;
-          }
-          
-          //ctx.fillStyle = "#fff";
-      	  //ctx.fillRect(j*scale-5, i*scale, scale-10, scale-5);
-      	  break;
-      	default:
-      	  ctx.fillStyle = "#0c7";
-      	  ctx.fillRect(j*scale, i*scale, scale, scale);
-      	  break;
+
+      var currentSpace = parkingLot[i][j];
+      var key = currentSpace.substring(0,1);
+
+      if (key == "w")
+        drawWall(ctx, i, j);
+      else if (key == "e" || key == "x" || key == "r")
+        drawRoad(ctx, i, j, currentSpace.substring(1,2));
+      else if (key == "c") {
+        drawRoad(ctx, i, j, currentSpace.substring(1,2));
+        drawCar(ctx, i, j, currentSpace.substring(1,2));
       }
+      else if (key == "p")
+        drawParkingSpace(ctx, i, j, currentSpace.substring(1,2));
+      else if (key == "f") {
+        drawParkingSpace(ctx, i, j, currentSpace.substring(1,2));
+        drawCar(ctx, i, j, currentSpace.substring(1,2), true);
+      }
+      else
+        drawTarget(ctx, i, j);
+        
     }
   }
 }
